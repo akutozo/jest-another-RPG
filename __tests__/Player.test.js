e119ac9aaab8e2f189b24dfbef5cd8ca1e281017
Jacob Daughtry <akutozo@gmail.com>
2020-08-05 20:51:49 -0500

    End of 10.2.7

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,7 @@
 const Player = require('../lib/Player');
 
 jest.mock('../lib/Potion');
-
+//Test 1
 test('creates a player object', () => {
     const player = new Player('Dave');
   
@@ -11,3 +11,26 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
     expect.arrayContaining([expect.any(Object)]);
   });
+
+//Test 2
+test("gets player's stats as an object", () => {
+const player = new Player('Dave');
+
+expect(player.getStats()).toHaveProperty('potions');
+expect(player.getStats()).toHaveProperty('health');
+expect(player.getStats()).toHaveProperty('strength');
+expect(player.getStats()).toHaveProperty('agility');
+});
+
+//Test 3
+test('gets inventory from player or returns false', () => {
+const player = new Player('Dave');
+
+expect(player.getInventory()).toEqual(expect.any(Array));
+
+player.inventory = [];
+
+expect(player.getInventory()).toEqual(false);
+});
+
+//Test 4
